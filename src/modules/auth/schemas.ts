dad03678859c69166e0205f857ecd63025f23355
Matheus <matheusim27@hotmail.com>
2025-05-04 20:02:19 -0300

    improv: auth schemas types

--- src/modules/auth/schemas.ts
+++ src/modules/auth/schemas.ts
@@ -6,7 +6,11 @@ export const registerSchema = z.object({
   password: z.string().min(6, 'Senha deve ter no mínimo 6 caracteres').max(8, 'A Senha deve ter no máximo 8 caracteres'),
 });
 
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
+
 export const loginSchema = z.object({
   email: z.string().email('E-mail inválido'),
   password: z.string().min(6, 'Senha inválida'),
 });
+
+export type LoginSchemaType = z.infer<typeof loginSchema>;
